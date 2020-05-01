7b7a74ba24cc7e23e3e183c67a0a7bea86a9f706
Michelle Lu <lu.michelle96@icloud.com>
2020-05-01 00:22:22 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,10 +12,15 @@ class App < Sinatra::Base
   end
 
 
-  get '/say/:number/:phrase' do
-    @number = params[:number].to_i
-    @phrase = params[:phrase]
-    erb :say
+  get "/say/:number/:phrase" do
+    @the_product = ""
+    @the_number = params[:number].to_i
+    @the_phrase = params[:phrase]
+    @the_number.times do
+      @the_product += @the_phrase
+      @the_product += "\n"
+    end
+    "#{@the_product}"
   end
 
   get '/say/:word1/:word2/:word3/:word4/:word5' do
