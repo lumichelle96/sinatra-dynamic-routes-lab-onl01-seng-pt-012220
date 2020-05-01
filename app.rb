1a1b6f41a832fc40a1296e34b13915034865969b
Michelle Lu <lu.michelle96@icloud.com>
2020-05-01 00:19:02 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,6 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  # Write your code here!
+  get '/reversename/:name' do
+    @name = params[:name]
+    erb :reversename
+  end
+  
+  get '/square/:number' do
+    @num = params[:number].to_i
+    erb :square
+  end
+  
+  get
 
 end
